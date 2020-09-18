35633251eb28f0c6206c716a5e9143056c7d11dd
Em Lim <emily.lim13@gmail.com>
2020-09-18 12:53:38 -0700

    Write code to start analyzing community data

---
+++ Aquatic_Conservation_Group/Code/Community_Analysis.R
@@ -0,0 +1,18 @@
+# September 18, 2020
+# Code to analyze the Aquatic Conservation Group Project
+# Written by Em Lim
+
+# Load packages and data -----
+
+library(ggplot2)
+library(tidyverse)
+library(vegan)
+library(lme4)
+library(lmerTest)
+
+species_data <- read.csv("2020_Sept_species_x_site.csv")
+site_data <- read.csv("2020_Sept_site_x_environment.csv")
+
+# Species Richness -----
+site_data$shannon <- (diversity(species_data, index = "shannon")) #makes a new column in site data with the shannon values
+site_data$simpson <- (diversity(species_data, index = "simpson"))
