ce0c383072bf03e025731ed16cb62a993625cece
Em Lim <emily.lim13@gmail.com>
2020-09-18 15:44:58 -0700

    add package to read files from google drive!

--- Aquatic_Conservation_Group/Code/Community_Analysis.R
+++ Aquatic_Conservation_Group/Code/Community_Analysis.R
@@ -9,9 +9,20 @@ library(tidyverse)
 library(vegan)
 library(lme4)
 library(lmerTest)
+install.packages("googlesheets4")
+library(googlesheets4)
 
 species_data <- read.csv("2020_Sept_species_x_site.csv")
 site_data <- read.csv("2020_Sept_site_x_environment.csv")
+
+# Read sheets from google drive
+site <- googledrive::drive_get("2020_Sept_site_x_environment") %>% 
+  read_sheet()
+site_data <- as.data.frame(site)
+
+species <- googledrive::drive_get("2020_Sept_species_x_site") %>%
+  read_sheet()
+species_data <- as.data.frame(species)
 
 # Species Richness -----
 site_data$shannon <- (diversity(species_data, index = "shannon")) #makes a new column in site data with the shannon values
