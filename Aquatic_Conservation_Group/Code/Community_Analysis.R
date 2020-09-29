6628fdcd9defc64ab70288621303b8b6a145c209
Em Lim <emily.lim13@gmail.com>
2020-09-29 13:50:12 -0700

    tidying up the graphing section

--- Aquatic_Conservation_Group/Code/Community_Analysis.R
+++ Aquatic_Conservation_Group/Code/Community_Analysis.R
@@ -70,10 +70,6 @@ myNMDS <- metaMDS(species_data, k = 2)
 myNMDS #most important: is the stress low? Here it is >0.2 whihc is a bit on the high side
 stressplot(new_NMDS) #low stress means that the observed dissimilarity between site pairs matches that on the 2-D plot fairly well (points hug the line)
 
-# link for graphing help
-# https://rpubs.com/CPEL/NMDS
-# https://www.rpubs.com/RGrieger/545184
-
 #ugly plot
 ordiplot(myNMDS, type = "n") 
 ordihull(myNMDS, groups = site_data$beach,draw = "polygon",col = "grey99",label = T)
@@ -82,12 +78,15 @@ orditorp(myNMDS, display = "sites", cex = 0.75, air = 0.01)
 
 
 # Ordination plot with ggplot -----
-#https://www.rpubs.com/RGrieger/545184
+# link for graphing help
+# https://rpubs.com/CPEL/NMDS
+# https://www.rpubs.com/RGrieger/545184
 
-set.seed(604671)
+set.seed(604671825)
 myNMDS <- metaMDS(species_data, k = 2)
 my_envfit <- envfit(myNMDS, site_data, permutations = 999)
 spp_fit <- envfit(myNMDS, species_data, permutations = 999)
+
 
 #save NMDS results into dataframe
 site_scrs <- as.data.frame(scores(myNMDS, display = "sites")) 
@@ -98,15 +97,10 @@ site_scrs <- cbind(site_scrs, Beach = site_data$beach)
 #add grouping variable of cluster grouping to dataframe
 site_scrs <- cbind(site_scrs, Management = site_data$management) 
 
-head(site_scrs)
-
 spp_scrs <- as.data.frame(scores(spp_fit, display = "vectors")) #save species intrinsic values into dataframe
 spp_scrs <- cbind(spp_scrs, Species = rownames(spp_scrs)) #add species names to dataframe
 spp_scrs <- cbind(spp_scrs, pval = spp_fit$vectors$pvals) #add pvalues to dataframe so you can select species which are significant
 #spp.scrs<- cbind(spp.scrs, abrev = abbreviate(spp.scrs$Species, minlength = 6)) #abbreviate species names
-sig_spp_scrs <- subset(spp_scrs, pval<=0.05) #subset data to show species significant at 0.05
-
-head(spp_scrs)
 
 env_scores <- as.data.frame(scores(my_envfit, display = "vectors")) #extracts relevant scores from envifit
 env_scores <- cbind(env_scores, env.variables = rownames(env_scores)) #and then gives them their names
@@ -115,30 +109,11 @@ env_scores <- cbind(env_scores, pval = my_envfit$vectors$pvals) # add pvalues to
 sig_env_scrs <- subset(env_scores, pval<=0.05) #subset data to show variables significant at 0.05
   # there are no significant variables bc all our data is categorical except distance, which we don't actually care about
 
-head(env_scores)
-
-#plot
-theme_set(theme_classic(base_size = 28)) # set default settings
-
-nmds_plot <- ggplot(site_scrs, aes(x=NMDS1, y=NMDS2))+ #sets up the plot
-  geom_point(aes(NMDS1, NMDS2, colour = factor(Beach), shape = factor(Management)), size = 2)+ #adds site points to plot, shape determined by management, colour determined by beach ID
-  coord_fixed()+
-  theme_classic()+ 
-  theme(panel.background = element_rect(fill = NA, colour = "black", size = 1, linetype = "solid"))+
-  labs(colour = "Beach", shape = "Management")+ # add legend labels for Management and Beach
-  theme(legend.position = "right", legend.text = element_text(size = 20, colour = "black"), legend.title = element_text(size = 20, colour = "black"), axis.text = element_text(size = 20, colour = "black")) # add legend at right of plot
-
 # make polygons
 ordi_hull <- 
   site_scrs %>% 
   group_by(Beach) %>% 
   slice(chull(NMDS1, NMDS2))
-
-nmds_plot1 <- nmds_plot + geom_polygon(data = ordi_hull,
-                                        aes(fill = Beach),
-                                        alpha = 0.2,
-                                        show.legend = FALSE)
-print(nmds_plot1)
 
 
 
@@ -160,13 +135,41 @@ images <- c("ian-symbol-sponge-1.png", "ian-symbol-amphipod.png","ian-symbol-pac
 #add image names to dataframe
 signif_spp_scrs <- cbind(signif_spp_scrs_cut, images)
 
-# add species images
-nmds_plot2 <- nmds_plot1 + geom_image(data = signif_spp_scrs, by = "height", aes(x = NMDS1, y = NMDS2, image = images), size = 0.08)
+#cut species that weren't significant
+sig_spp_scrs <- subset(signif_spp_scrs, pval<=0.05) #subset data to show species significant at 0.05
 
 
-print(nmds_plot2)
+# make the plot
+theme_set(theme_classic(base_size = 28)) # set default settings
 
-ggsave("../Figures/ordination.png", device = "png",
+nmds_plot <- ggplot(site_scrs, aes(x=NMDS1, y=NMDS2))+ #sets up the plot
+  geom_point(aes(NMDS1, NMDS2, colour = factor(Beach), shape = factor(Management)), size = 2)+ #adds site points to plot, shape determined by management, colour determined by beach ID
+  coord_fixed()+
+  theme_classic()+ 
+  theme(panel.background = element_rect(fill = NA, colour = "black", size = 1, linetype = "solid"))+
+  labs(colour = "Beach", shape = "Management")+ # add legend labels for Management and Beach
+  theme(legend.position = "right", legend.text = element_text(size = 20, colour = "black"), legend.title = element_text(size = 20, colour = "black"), axis.text = element_text(size = 20, colour = "black")) # add legend at right of plot
+
+#add polygons
+nmds_plot1 <- nmds_plot + geom_polygon(data = ordi_hull,
+                                        aes(fill = Beach),
+                                        alpha = 0.2,
+                                        show.legend = FALSE)
+
+# add just sig species images
+nmds_plot_sig <- nmds_plot1 + geom_image(data = sig_spp_scrs, by = "height", aes(x = NMDS1, y = NMDS2, image = images), size = 0.08)
+
+# add all species images
+nmds_plot_all <- nmds_plot1 + geom_image(data = signif_spp_scrs, by = "height", aes(x = NMDS1, y = NMDS2, image = images), size = 0.08)
+
+
+print(nmds_plot_sig)
+ggsave("../Figures/sig_ordination.png", device = "png",
+       height = 9, width = 16, dpi = 400)
+
+print(nmds_plot_all)
+
+ggsave("../Figures/all_ordination.png", device = "png",
        height = 9, width = 16, dpi = 400)
 
 
